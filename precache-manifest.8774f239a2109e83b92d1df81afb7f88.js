2a409688aed3b8dc521f04d2da68b72e1b6b32e1
omprakash <routomprakash0@gmail.com>
2023-02-08 08:32:10 +0530

    Updates

---
+++ precache-manifest.8774f239a2109e83b92d1df81afb7f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd3f594640000ef10140",
+    "url": "/react-live-website/static/css/main.ddd074e2.chunk.css"
+  },
+  {
+    "revision": "fd3f594640000ef10140",
+    "url": "/react-live-website/static/js/main.fd3f5946.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-live-website/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/react-live-website/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "f4852da939a4df776351",
+    "url": "/react-live-website/static/js/runtime~main.f4852da9.js"
+  },
+  {
+    "revision": "1627e0fe96880b2710426edb99f3b6db",
+    "url": "/react-live-website/index.html"
+  }
+];
